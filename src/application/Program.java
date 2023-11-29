a2d3d8c6bb7665c721a51c8ce8c8868f59d72d30
DaniloParavani <daniloparavani@yahoo.com.br>
2023-11-28 23:33:54 -0300

    Department insert and findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -15,41 +16,54 @@ public class Program {
 
         Scanner sc = new Scanner(System.in);
 
-        SellerDao sellerDao = DaoFactory.createSellerDao();
+//        SellerDao sellerDao = DaoFactory.createSellerDao();
+//
+//        System.out.println("==== TEST 1: seller findById ====");
+//        Seller seller = sellerDao.findById(3);
+//        System.out.println(seller);
+//
+//        System.out.println("\n==== TEST 2: seller findByDepartment ====");
+//        Department department = new Department(2, null);
+//        List<Seller> list = sellerDao.findByDepartment(department);
+//        for (Seller obj : list) {
+//            System.out.println(obj);
+//        }
+//
+//        System.out.println("\n==== TEST 3: seller findAll ====");
+//        list = sellerDao.findAll();
+//        for (Seller obj : list) {
+//            System.out.println(obj);
+//        }
+//
+//        System.out.println("\n==== TEST 4: seller insert ====");
+//        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+//        sellerDao.insert(newSeller);
+//        System.out.println("Inserted! New id = " + newSeller.getId());
+//
+//        System.out.println("\n==== TEST 5: seller update ====");
+//        seller = sellerDao.findById(1);
+//        seller.setName("Martha Waine");
+//        sellerDao.update(seller);
+//        System.out.println("Update completed!");
+//
+//        System.out.println("\n==== TEST 6: seller delete ====");
+//        System.out.println("Enter id for delete test: ");
+//        int id = sc.nextInt();
+//        sellerDao.deleteById(id);
+//        System.out.println("Delete completed!");
 
-        System.out.println("==== TEST 1: seller findById ====");
-        Seller seller = sellerDao.findById(3);
-        System.out.println(seller);
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        System.out.println("\n==== TEST 2: seller findByDepartment ====");
-        Department department = new Department(2, null);
-        List<Seller> list = sellerDao.findByDepartment(department);
-        for (Seller obj : list) {
-            System.out.println(obj);
-        }
+//        System.out.println("\n==== TEST 7: department insert ====");
+//        Department newDepartment = new Department(null, "Technology 2");
+//        departmentDao.insert(newDepartment);
+//        System.out.println("Inserted! New id = " + newDepartment.getId());
 
-        System.out.println("\n==== TEST 3: seller findAll ====");
-        list = sellerDao.findByDepartment(department);
-        for (Seller obj : list) {
-            System.out.println(obj);
-        }
+//        System.out.println("\n==== TEST 8: department update ====");
 
-        System.out.println("\n==== TEST 4: seller insert ====");
-        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-        sellerDao.insert(newSeller);
-        System.out.println("Inserted! New id = " + newSeller.getId());
-
-        System.out.println("\n==== TEST 5: seller update ====");
-        seller = sellerDao.findById(1);
-        seller.setName("Martha Waine");
-        sellerDao.update(seller);
-        System.out.println("Update completed!");
-
-        System.out.println("\n==== TEST 6: seller delete ====");
-        System.out.println("Enter id for delete test: ");
-        int id = sc.nextInt();
-        sellerDao.deleteById(id);
-        System.out.println("Delete completed!");
+        System.out.println("==== TEST 9: department findById ====");
+        Department department = departmentDao.findById(3);
+        System.out.println(department);
 
         sc.close();
     }
